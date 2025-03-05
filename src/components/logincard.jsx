5463d2518b3ffa531ec3ed28598038c048ad54ff
Aryandeo8 <aryandeo@gmail.com>
2025-03-05 23:50:20 +0530

    styling

--- src/components/logincard.jsx
+++ src/components/logincard.jsx
@@ -10,8 +10,8 @@ export const Logincard = () => {
   };
   return (
 
-    <div className=''>  
-    <div>
+    <div className="min-h-50 flex items-center justify-center">  
+    <div className="bg-white/10 backdrop-blur-sm rounded-2xl p-8 max-w-md w-full mx-4 border-2 border-white/20 shadow-lg "> 
       <h2>Welcome</h2>
       <form action="">
         {/* Form */}
@@ -29,8 +29,30 @@ export const Logincard = () => {
         {/* enter password */}
         <div>
           <label className="">password</label>
-          input 
+          <div>
+          <input 
+          type={showPassword ? 'text' : 'password'}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className=""
+          placeholder="Enter your password" 
+          />
+          <button
+          type="button"
+          onClick={() => setShowPassword(!showPassword)}
+          className=""
+          >{showPassword ? '👁️' : '👁️🗨️'}
+          </button>
+          </div>
         </div>
+          <div className=''>
+            <button className=''>
+              <div className=''>
+                <span className=''></span>
+              </div>
+              <span className=''></span>
+            </button>
+          </div>
       </form>
     </div>
     </div>
