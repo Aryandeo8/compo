cf6a8d6f5d0c2d623267c5b5bbcfece6344abf44
Aryandeo8 <aryandeo@gmail.com>
2025-03-06 15:42:14 +0530

    starting a card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { Login } from './pages/login';
+import { display } from './pages/display';
+import {Routes, Route} from "react-router-dom"
 function App () {
   return (
-    <Login/>
-  )
+    <Routes>
+      <Route path="/" element={<Login/>} />
+      <Route path="/display" element={<display/>}/>
+    </Routes>
+    
+  );
 }
 export default App;
 
